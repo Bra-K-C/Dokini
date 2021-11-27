02eec5b3d3dbdefe7fcec5f4dc8963a922f7b497
MathildeCop1 <matcop38@gmail.com>
2021-11-27 19:06:53 +0100

    css seasons

--- pages/season_food.php
+++ pages/season_food.php
@@ -1,70 +1,78 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <?php session_start()?>
-<html>
+<html lang="fr">
 <head>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="../ccs/seasons.css" rel="stylesheet">
-    <script>
-        function openNav() {
-            document.getElementById("mySidebar").style.width = "250px";
-            document.getElementById("main").style.marginLeft = "250px";
-        }
+  <title class="titre">Fruits et légumes</title>
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="../ccs/seasons.css" rel="stylesheet">
+  <script>
+    function openNav() {
+      document.getElementById("mySidebar").style.width = "250px";
+      document.getElementById("main").style.marginLeft = "250px";
+      }
 
-        function closeNav() {
-            document.getElementById("mySidebar").style.width = "0";
-            document.getElementById("main").style.marginLeft= "0";
-        }
-
-    </script>
+    function closeNav() {
+      document.getElementById("mySidebar").style.width = "0";
+      document.getElementById("main").style.marginLeft= "0";
+    }
+  </script>
 </head>
+
 <body>
-<div id="main">
+  <header>
+    <h1>
+      <img class="wallpaper" src="\IMG\fruitetleg.jpg">
+      <span class="titre">Fruits et légumes de saison</span>
+    </h1>
+  </header>
+
+  <div id="main">
     <button class="openbtn" onclick="openNav()">☰ Ouvrir barre de navigation</button>
-    <h2>Fruits et légumes de saisons</h2>
-<?php
-            $month = $_SESSION['month'];
-            echo'<p>'.$_SESSION['month'].'</p>';
+    <?php
+      $month = $_SESSION['month'];
+      echo'<p>'.$_SESSION['month'].'</p>';
 
-            $fh = fopen("../seasons/saisons.txt", 'r');
-            $line = fgets($fh);
-            for ($x = 0; $x < $month; $x++)
-                $line = fgets($fh);
+      $fh = fopen("../seasons/saisons.txt", 'r');
+      $line = fgets($fh);
+      for ($x = 0; $x < $month; $x++)
+        $line = fgets($fh);
 
-            $vegetables = [];
-            $i = 0;
-            $c = $line[$i];
-            $isWord = false;
-            $isLabel = false;
-            $str = "";
-            while ($c !== ']') {
-                if ($c === '\'') {
-                    $isWord = !$isWord;
-                    if ($str !== ""){
-                        array_push($vegetables, $str);
-                        echo'<p>Nutriment : '.$str.'</p>';
-                        $isLabel = !$isLabel;
-                    }
-                    $str = "";
-                }
-                if ($isWord && $line[$i] !== '\'') {
-                    $str .= $c;
-                }
-                if ($c === ',') {
-                    if ($str !== "") {
-                        array_push($vegetables, $str);
-                        echo '<p>Local : ' . $str .'</p>';
-                        $c = '\'';
-                        $isLabel = !$isLabel;
-                    }
-                    $str = "";
-                }
-                if ($isLabel && $line[$i] !== ',' && $c !== '\'') {
-                    $str .= $c;
-                }
-                $i++;
-                $c = $line[$i];
-            }
+      $vegetables = [];
+      $i = 0;
+      $c = $line[$i];
+      $isWord = false;
+      $isLabel = false;
+      $str = "";
+      while ($c !== ']') {
+        if ($c === '\'') {
+          $isWord = !$isWord;
+          if ($str !== ""){
+            array_push($vegetables, $str);
+            echo'<p>Nutriment : '.$str.'</p>';
+            $isLabel = !$isLabel;
+          }
+          $str = "";
+        }
+        if ($isWord && $line[$i] !== '\'') {
+          $str .= $c;
+        }
+        if ($c === ',') {
+          if ($str !== "") {
+            array_push($vegetables, $str);
+            echo '<p>Local : ' . $str .'</p>';
+            $c = '\'';
+            $isLabel = !$isLabel;
+          }
+          $str = "";
+        }
+        if ($isLabel && $line[$i] !== ',' && $c !== '\'') {
+          $str .= $c;
+        }
+        $i++;
+        $c = $line[$i];
+        }
 ?>
 </div>
 <div id="mySidebar" class="sidebar">
@@ -82,5 +90,4 @@
     <a onclick="closeNav()" href="">Novembre</a>
     <a onclick="closeNav()" href="">Décembre</a>
 </div>
-<div></div>
 </body>
