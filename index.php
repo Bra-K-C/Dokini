8bd7431ae0943081d20745b532f44ffe0a8ffb62
MathildeCop1 <matcop38@gmail.com>
2021-11-27 04:43:29 +0100

    3.1.0

--- index.php
+++ index.php
@@ -9,35 +9,30 @@
 </head>
 
 <body>
-<nav class="menu" style="height: 60px;">
-        <h1><img src="\IMG\ebauche_logo.jpg" align="left"  class="logo">  &nbsp  Dokini</h1>
-    <ul class="links">
+  <header>
+    <h1>
+        <img class="logo" src="\IMG\ebauche_logo.jpg">
+        Dokini
+    </h1>
+  </header>
 
-            <li>
-                <a href="pages/login.php">Connexion |</a>
-            </li>
-            <li>
-                <a href="pages/register.php"> Inscription</a>
-            </li>
-    </ul>
+  <nav>
+    <a class="navig" href="pages/login.php">Connexion</a>
+    <a class="navig" href="pages/register.php"> Inscription</a>
+  </nav>
 
+  <h2>Dokini, c'est quoi?</h2>
+    <p>
+      Dokini est un site web développé par des étudiants pour permettre de manger
+      mieux tout en préservant l'environnement.
+    </p>
+    <br />
 
-
-
-</nav>
-<header><h1>Dokini</h1></header>
-
-<h2>Dokini, c'est quoi?</h2>
-<p>
-    Dokini est un site web développé par des étudiants pour permettre de manger
-    mieux tout en préservant l'environnement.
-</p>
-<br />
-<p>
-    Dokini vous propose diverses recettes selon vos goûts et vos besoins.
-    Mais pas seulement! Les recettes varient selon la période de l'année
-    pour vous permettre de manger des produits de saison. Dokini vous permet
-    aussi de sélectionner des recettes et vous crée votre liste de courses. Tout
-    est plus simple
-</p>
+    <p>
+      Dokini vous propose diverses recettes selon vos goûts et vos besoins.
+      Mais pas seulement! Les recettes varient selon la période de l'année
+      pour vous permettre de manger des produits de saison. Dokini vous permet
+      aussi de sélectionner des recettes et vous crée votre liste de courses. Tout
+      est plus simple
+    </p>
 </body>
